837aab2b01cba8d25aea746aa24b20895327c33c
Chris Bush <chris.bush@mongodb.com>
2020-09-25 15:45:46 -0400

    Final state

---
+++ Task Tracker/WelcomeViewController.swift
@@ -0,0 +1,177 @@
+//
+//  WelcomeViewController.swift
+//  Task Tracker
+//
+//  Created by MongoDB on 2020-04-30.
+//  Copyright © 2020 MongoDB, Inc. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+// The WelcomeViewController handles login and account creation.
+class WelcomeViewController: UIViewController {
+    let emailField = UITextField()
+    let passwordField = UITextField()
+    let signInButton = UIButton(type: .roundedRect)
+    let signUpButton = UIButton(type: .roundedRect)
+    let errorLabel = UILabel()
+    let activityIndicator = UIActivityIndicatorView(style: .medium)
+
+    var email: String? {
+        get {
+            return emailField.text
+        }
+    }
+
+    var password: String? {
+        get {
+            return passwordField.text
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        view.backgroundColor = .white
+
+        // Create a view that will automatically lay out the other controls.
+        let container = UIStackView();
+        container.translatesAutoresizingMaskIntoConstraints = false
+        container.axis = .vertical
+        container.alignment = .fill
+        container.spacing = 16.0;
+        view.addSubview(container)
+
+        // Configure the activity indicator.
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(activityIndicator)
+
+        // Set the layout constraints of the container view and the activity indicator.
+        let guide = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            // This pins the container view to the top and stretches it to fill the parent
+            // view horizontally.
+            container.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 16),
+            container.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -16),
+            container.topAnchor.constraint(equalTo: guide.topAnchor, constant: 16),
+            // The activity indicator is centered over the rest of the view.
+            activityIndicator.centerYAnchor.constraint(equalTo: guide.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: guide.centerXAnchor),
+            ])
+
+        // Add some text at the top of the view to explain what to do.
+        let infoLabel = UILabel()
+        infoLabel.numberOfLines = 0
+        infoLabel.text = "Please enter a email and password."
+        container.addArrangedSubview(infoLabel)
+
+        // Configure the email and password text input fields.
+        emailField.placeholder = "Username"
+        emailField.borderStyle = .roundedRect
+        emailField.autocapitalizationType = .none
+        emailField.autocorrectionType = .no
+        container.addArrangedSubview(emailField)
+
+        passwordField.placeholder = "Password"
+        passwordField.isSecureTextEntry = true
+        passwordField.borderStyle = .roundedRect
+        container.addArrangedSubview(passwordField)
+
+        // Configure the sign in and sign up buttons.
+        signInButton.setTitle("Sign In", for: .normal);
+        signInButton.addTarget(self, action: #selector(signIn), for: .touchUpInside)
+        container.addArrangedSubview(signInButton)
+
+        signUpButton.setTitle("Sign Up", for: .normal);
+        signUpButton.addTarget(self, action: #selector(signUp), for: .touchUpInside)
+        container.addArrangedSubview(signUpButton)
+
+        // Error messages will be set on the errorLabel.
+        errorLabel.numberOfLines = 0
+        errorLabel.textColor = .red
+        container.addArrangedSubview(errorLabel)
+    }
+
+    // Turn on or off the activity indicator.
+    func setLoading(_ loading: Bool) {
+        if loading {
+            activityIndicator.startAnimating();
+            errorLabel.text = "";
+        } else {
+            activityIndicator.stopAnimating();
+        }
+        emailField.isEnabled = !loading
+        passwordField.isEnabled = !loading
+        signInButton.isEnabled = !loading
+        signUpButton.isEnabled = !loading
+    }
+
+    @objc func signUp() {
+        setLoading(true);
+        app.emailPasswordAuth.registerUser(email: email!, password: password!, completion: { [weak self](error) in
+            // Completion handlers are not necessarily called on the UI thread.
+            // This call to DispatchQueue.main.sync ensures that any changes to the UI,
+            // namely disabling the loading indicator and navigating to the next page,
+            // are handled on the UI thread:
+            DispatchQueue.main.sync {
+                self!.setLoading(false);
+                guard error == nil else {
+                    print("Signup failed: \(error!)")
+                    self!.errorLabel.text = "Signup failed: \(error!.localizedDescription)"
+                    return
+                }
+                print("Signup successful!")
+
+                // Registering just registers. Now we need to sign in, but we can reuse the existing email and password.
+                self!.errorLabel.text = "Signup successful! Signing in..."
+                self!.signIn()
+            }
+        })
+    }
+
+    @objc func signIn() {
+        print("Log in as user: \(email!)");
+        setLoading(true);
+
+        app.login(credentials: Credentials.emailPassword(email: email!, password: password!)) { [weak self](maybeUser, error) in
+            // Completion handlers are not necessarily called on the UI thread.
+            // This call to DispatchQueue.main.sync ensures that any changes to the UI,
+            // namely disabling the loading indicator and navigating to the next page,
+            // are handled on the UI thread:
+            DispatchQueue.main.sync {
+                self!.setLoading(false);
+                guard error == nil else {
+                    // Auth error: user already exists? Try logging in as that user.
+                    print("Login failed: \(error!)");
+                    self!.errorLabel.text = "Login failed: \(error!.localizedDescription)"
+                    return
+                }
+
+                guard let user = maybeUser else {
+                    fatalError("Invalid user object?")
+                }
+
+                print("Login succeeded!");
+
+                self!.setLoading(true);
+
+                // Get a configuration to open the synced realm.
+                var configuration = user.configuration(partitionValue: "user=\(user.id)")
+                // Only allow User objects in this partition.
+                configuration.objectTypes = [User.self, Project.self]
+                // Open the realm asynchronously so that it downloads the remote copy before
+                // opening the local copy.
+                Realm.asyncOpen(configuration: configuration) { [weak self](userRealm, error) in
+                    self!.setLoading(false);
+                    guard error == nil else {
+                        fatalError("Failed to open realm: \(error!)")
+                    }
+
+                    // For the second phase of the tutorial, go to the Projects management page.
+                    // This is where you can manage permissions and collaborators.
+                    self!.navigationController!.pushViewController(ProjectsViewController(userRealm: userRealm!), animated: true);
+                }
+            }
+        };
+    }
+}
