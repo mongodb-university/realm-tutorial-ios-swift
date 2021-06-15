cec62a27afbd2e0461d3a774e8653bec8f57cbab
Arty Fact <chris.bush+arty-fact@mongodb.com>
2021-06-15 15:15:33 +0000

    Update

--- Task Tracker/WelcomeViewController.swift
+++ Task Tracker/WelcomeViewController.swift
@@ -11,24 +11,17 @@ import RealmSwift
 
 // The WelcomeViewController handles login and account creation.
 class WelcomeViewController: UIViewController {
-    let emailField = UITextField()
-    let passwordField = UITextField()
+    let usernameField = UITextField()
     let signInButton = UIButton(type: .roundedRect)
-    let signUpButton = UIButton(type: .roundedRect)
     let errorLabel = UILabel()
     let activityIndicator = UIActivityIndicatorView(style: .medium)
 
-    var email: String? {
+    var username: String? {
         get {
-            return emailField.text
+            return usernameField.text
         }
     }
 
-    var password: String? {
-        get {
-            return passwordField.text
-        }
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,30 +55,21 @@ class WelcomeViewController: UIViewController {
         // Add some text at the top of the view to explain what to do.
         let infoLabel = UILabel()
         infoLabel.numberOfLines = 0
-        infoLabel.text = "Please enter a email and password."
         container.addArrangedSubview(infoLabel)
 
-        // Configure the email and password text input fields.
-        emailField.placeholder = "Username"
-        emailField.borderStyle = .roundedRect
-        emailField.autocapitalizationType = .none
-        emailField.autocorrectionType = .no
-        container.addArrangedSubview(emailField)
+        // Configure the username text input field.
+        usernameField.borderStyle = .roundedRect
+        usernameField.autocapitalizationType = .none
+        usernameField.autocorrectionType = .no
+        container.addArrangedSubview(usernameField)
 
-        passwordField.placeholder = "Password"
-        passwordField.isSecureTextEntry = true
-        passwordField.borderStyle = .roundedRect
-        container.addArrangedSubview(passwordField)
-
-        // Configure the sign in and sign up buttons.
+        
+        // Configure the sign in button.
         signInButton.setTitle("Sign In", for: .normal)
         signInButton.addTarget(self, action: #selector(signIn), for: .touchUpInside)
         container.addArrangedSubview(signInButton)
 
-        signUpButton.setTitle("Sign Up", for: .normal)
-        signUpButton.addTarget(self, action: #selector(signUp), for: .touchUpInside)
-        container.addArrangedSubview(signUpButton)
-
+        
         // Error messages will be set on the errorLabel.
         errorLabel.numberOfLines = 0
         errorLabel.textColor = .red
@@ -100,77 +84,12 @@ class WelcomeViewController: UIViewController {
         } else {
             activityIndicator.stopAnimating()
         }
-        emailField.isEnabled = !loading
-        passwordField.isEnabled = !loading
+        
+        usernameField.isEnabled = !loading
         signInButton.isEnabled = !loading
-        signUpButton.isEnabled = !loading
     }
 
-    @objc func signUp() {
-        setLoading(true)
-        app.emailPasswordAuth.registerUser(email: email!, password: password!, completion: { [weak self](error) in
-            // Completion handlers are not necessarily called on the UI thread.
-            // This call to DispatchQueue.main.async ensures that any changes to the UI,
-            // namely disabling the loading indicator and navigating to the next page,
-            // are handled on the UI thread:
-            DispatchQueue.main.async {
-                self!.setLoading(false)
-                guard error == nil else {
-                    print("Signup failed: \(error!)")
-                    self!.errorLabel.text = "Signup failed: \(error!.localizedDescription)"
-                    return
-                }
-                print("Signup successful!")
-
-                // Registering just registers. Now we need to sign in, but we can reuse the existing email and password.
-                self!.errorLabel.text = "Signup successful! Signing in..."
-                self!.signIn()
-            }
-        })
-    }
 
     @objc func signIn() {
-        print("Log in as user: \(email!)")
-        setLoading(true)
-
-        app.login(credentials: Credentials.emailPassword(email: email!, password: password!)) { [weak self](result) in
-            // Completion handlers are not necessarily called on the UI thread.
-            // This call to DispatchQueue.main.async ensures that any changes to the UI,
-            // namely disabling the loading indicator and navigating to the next page,
-            // are handled on the UI thread:
-            DispatchQueue.main.async {
-                self!.setLoading(false)
-                switch result {
-                case .failure(let error):
-                    // Auth error: user already exists? Try logging in as that user.
-                    print("Login failed: \(error)")
-                    self!.errorLabel.text = "Login failed: \(error.localizedDescription)"
-                    return
-                case .success(let user):
-                    print("Login succeeded!")
-
-                    // Load again while we open the realm.
-                    self!.setLoading(true)
-                    // Get a configuration to open the synced realm.
-                    var configuration = user.configuration(partitionValue: "user=\(user.id)")
-                    // Only allow User objects in this partition.
-                    configuration.objectTypes = [User.self, Project.self]
-                    // Open the realm asynchronously so that it downloads the remote copy before
-                    // opening the local copy.
-                    Realm.asyncOpen(configuration: configuration) { [weak self](result) in
-                        DispatchQueue.main.async {
-                            self!.setLoading(false)
-                            switch result {
-                            case .failure(let error):
-                                fatalError("Failed to open realm: \(error)")
-                            case .success(let userRealm):
-                                // Go to the list of projects in the user object contained in the user realm.
-                                self!.navigationController!.pushViewController(ProjectsViewController(userRealm: userRealm), animated: true)
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
 }
