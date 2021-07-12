cd5c69532c1ef0d3c4e0716a0d04672c81f39e16
Arty Fact <chris.bush+arty-fact@mongodb.com>
2021-07-12 20:10:12 +0000

    Update Initiated by https://github.com/mongodb/docs-realm/commit/d761190267332ecd6908364f1b521eeefc884bc6

---
+++ Task Tracker/Models.swift
@@ -0,0 +1,67 @@
+//
+//  Models.swift
+//  Task Tracker
+//
+//  Created by MongoDB on 2020-05-07.
+//  Copyright © 2020-2021 MongoDB, Inc. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class User: Object {
+    @objc dynamic var _id: String = ""
+    @objc dynamic var name: String = ""
+    let memberOf = RealmSwift.List<Project>()
+    override static func primaryKey() -> String? {
+        return "_id"
+    }
+}
+class Project: EmbeddedObject {
+    @objc dynamic var name: String?
+    @objc dynamic var partition: String?
+    convenience init(partition: String, name: String) {
+        self.init()
+        self.partition = partition
+        self.name = name
+    }
+}
+
+enum TaskStatus: String {
+  case Open
+  case InProgress
+  case Complete
+}
+
+class Task: Object {
+    @objc dynamic var _id: ObjectId = ObjectId.generate()
+    @objc dynamic var name: String = ""
+    @objc dynamic var owner: String?
+    @objc dynamic var status: String = ""
+    override static func primaryKey() -> String? {
+        return "_id"
+    }
+
+    var statusEnum: TaskStatus {
+        get {
+            return TaskStatus(rawValue: status) ?? .Open
+        }
+        set {
+            status = newValue.rawValue
+        }
+    }
+
+    convenience init(name: String) {
+        self.init()
+        self.name = name
+    }
+}
+
+struct Member {
+    let id: String
+    let name: String
+    init(document: Document) {
+        self.id = document["_id"]!!.stringValue!
+        self.name = document["name"]!!.stringValue!
+    }
+}
