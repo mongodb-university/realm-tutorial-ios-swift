71baab1c198aa2408b3417cfdcbb26e5ab542130
Arty Fact <chris.bush+arty-fact@mongodb.com>
2021-07-12 20:10:01 +0000

    Update Initiated by https://github.com/mongodb/docs-realm/commit/d761190267332ecd6908364f1b521eeefc884bc6

---
+++ Task Tracker/Models.swift
@@ -0,0 +1,32 @@
+//
+//  Models.swift
+//  Task Tracker
+//
+//  Created by MongoDB on 2020-05-07.
+//  Copyright © 2020-2021 MongoDB, Inc. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+
+enum TaskStatus: String {
+  case Open
+  case InProgress
+  case Complete
+}
+
+// TODO: Realm-ify Task model
+class Task {
+   var name: String = ""
+   var statusEnum: TaskStatus = .Open
+}
+
+struct Member {
+    let id: String
+    let name: String
+    init(document: Document) {
+        self.id = document["_id"]!!.stringValue!
+        self.name = document["name"]!!.stringValue!
+    }
+}
---
+++ Task Tracker/TasksViewController.swift
@@ -0,0 +1,135 @@
+//
+//  TasksViewController.swift
+//  Task Tracker
+//
+//  Created by MongoDB on 2020-05-07.
+//  Copyright © 2020-2021 MongoDB, Inc. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let tableView = UITableView()
+    let realm: Realm
+    var notificationToken: NotificationToken?
+    // TODO: Use Realm Results collection for `tasks`
+    let tasks: [Task] = []
+
+    required init(realmConfiguration: Realm.Configuration, title: String) {
+        self.realm = try! Realm(configuration: realmConfiguration)
+        
+        // TODO: initialize `tasks` with the collection of Tasks in the realm, sorted by _id.
+
+        super.init(nibName: nil, bundle: nil)
+
+        self.title = title
+
+        // TODO: Observe the tasks for changes. Hang on to the returned notification token.
+        // When changes are received, update the tableView.
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    deinit {
+        // TODO: invalidate notificationToken
+    }
+
+    override func viewDidLoad() {
+        // Configure the view.
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.frame = self.view.frame
+        view.addSubview(tableView)
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonDidClick))
+
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // This defines how the Tasks in the list look.
+        // We want the task name on the left and some indication of its status on the right.
+        let task = tasks[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") ?? UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        cell.selectionStyle = .none
+        cell.textLabel?.text = task.name
+        switch task.statusEnum {
+        case .Open:
+            cell.accessoryView = nil
+            cell.accessoryType = UITableViewCell.AccessoryType.none
+        case .InProgress:
+            let label = UILabel.init(frame: CGRect(x: 0, y: 0, width: 100, height: 20))
+            label.text = "In Progress"
+            cell.accessoryView = label
+        case .Complete:
+            cell.accessoryView = nil
+            cell.accessoryType = UITableViewCell.AccessoryType.checkmark
+        }
+        return cell
+    }
+
+    @objc func addButtonDidClick() {
+        let alertController = UIAlertController(title: "Add Task", message: "", preferredStyle: .alert)
+
+        // When the user clicks the add button, present them with a dialog to enter the task name.
+        alertController.addAction(UIAlertAction(title: "Save", style: .default, handler: {
+            _ -> Void in
+            let textField = alertController.textFields![0] as UITextField
+
+            
+            // TODO: Replace the following code with code to create a Task instance and add it to the realm in a write block.
+            let alertController = UIAlertController(title: "TODO", message: "Implement add task functionality in TasksViewController.swift", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alertController, animated: true)
+        }))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alertController.addTextField(configurationHandler: { (textField: UITextField!) -> Void in
+            textField.placeholder = "New Task Name"
+        })
+
+        // Show the dialog.
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        // User selected a task in the table. We will present a list of actions that the user can perform on this task.
+        let task = tasks[indexPath.row]
+
+        // Create the AlertController and add its actions.
+        let actionSheet: UIAlertController = UIAlertController(title: task.name, message: "Select an action", preferredStyle: .actionSheet)
+
+        // TODO: Populate the action sheet with task status update functions
+        // for every state the task is not currently in.
+
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel) { _ in
+                actionSheet.dismiss(animated: true)
+            })
+
+        // Show the actions list.
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+
+        // User can swipe to delete items.
+        let task = tasks[indexPath.row]
+
+        // TODO: Replace the following code with code to delete the task from the realm in a write block.
+        let alertController = UIAlertController(title: "TODO", message: "Implement delete functionality in TasksViewController.swift", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alertController, animated: true)
+    }
+
+
+}
