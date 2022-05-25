1513b2c5df774fb439f096570152c2e842c92889
Arnold Rozon <arnold@rozon.org>
2022-05-24 19:49:31 -0700

    Revert "Update" (#5)
    This reverts commit 8e0ad7c500090064523e3a48e27d95d46f7ac4e2.

--- Task Tracker/TasksViewController.swift
+++ Task Tracker/TasksViewController.swift
@@ -10,80 +10,32 @@ import RealmSwift
 
 class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    // :code-block-start: properties
     let tableView = UITableView()
     let realm: Realm
     var notificationToken: NotificationToken?
-    // :state-start: local sync
-    let tasks: Results<Task>
-    // :state-end: :state-uncomment-start: start
-    // // TODO: Use Realm Results collection for `tasks`
-    // let tasks: [Task] = []
-    // :state-uncomment-end:
-    // :code-block-end:
+    // TODO: Use Realm Results collection for `tasks`
+    let tasks: [Task] = []
 
-    // :code-block-start: init
     required init(realmConfiguration: Realm.Configuration, title: String) {
         self.realm = try! Realm(configuration: realmConfiguration)
         
-        // :state-start: local sync
-        // Access all tasks in the realm, sorted by _id so that the ordering is defined.
-        tasks = realm.objects(Task.self).sorted(byKeyPath: "_id")
-        // :state-end: :state-uncomment-start: start
-        // // TODO: initialize `tasks` with the collection of Tasks in the realm, sorted by _id.
-        // :state-uncomment-end:
+        // TODO: initialize `tasks` with the collection of Tasks in the realm, sorted by _id.
 
         super.init(nibName: nil, bundle: nil)
 
         self.title = title
 
-        // :state-start: local sync
-        // Observe the tasks for changes. Hang on to the returned notification token.
-        notificationToken = tasks.observe { [weak self] (changes) in
-            guard let tableView = self?.tableView else { return }
-            switch changes {
-            case .initial:
-                // Results are now populated and can be accessed without blocking the UI
-                tableView.reloadData()
-            case .update(_, let deletions, let insertions, let modifications):
-                // Query results have changed, so apply them to the UITableView.
-                tableView.performBatchUpdates({
-                    // It's important to be sure to always update a table in this order:
-                    // deletions, insertions, then updates. Otherwise, you could be unintentionally
-                    // updating at the wrong index!
-                    tableView.deleteRows(at: deletions.map({ IndexPath(row: $0, section: 0) }),
-                        with: .automatic)
-                    tableView.insertRows(at: insertions.map({ IndexPath(row: $0, section: 0) }),
-                        with: .automatic)
-                    tableView.reloadRows(at: modifications.map({ IndexPath(row: $0, section: 0) }),
-                        with: .automatic)
-                })
-            case .error(let error):
-                // An error occurred while opening the Realm file on the background worker thread
-                fatalError("\(error)")
-            }
-        }
-        // :state-end: :state-uncomment-start: start
-        // // TODO: Observe the tasks for changes. Hang on to the returned notification token.
-        // // When changes are received, update the tableView.
-        // :state-uncomment-end:
+        // TODO: Observe the tasks for changes. Hang on to the returned notification token.
+        // When changes are received, update the tableView.
     }
-    // :code-block-end:
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    // :code-block-start: deinit
     deinit {
-        // :state-start: local sync
-        // Always invalidate any notification tokens when you are done with them.
-        notificationToken?.invalidate()
-        // :state-end: :state-uncomment-start: start
-        // // TODO: invalidate notificationToken
-        // :state-uncomment-end:
+        // TODO: invalidate notificationToken
     }
-    // :code-block-end:
 
     override func viewDidLoad() {
         // Configure the view.
@@ -96,17 +48,6 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonDidClick))
 
-        // :code-block-start: check-if-own-tasks
-        // :state-uncomment-start: sync
-        // if isOwnTasks() {
-        //     // Only set up the manage team button if these are tasks the user owns.
-        //     toolbarItems = [
-        //         UIBarButtonItem(title: "Manage Team", style: .plain, target: self, action: #selector(manageTeamButtonDidClick))
-        //     ]
-        //     navigationController?.isToolbarHidden = false
-        // }
-        // :state-uncomment-end:
-        // :code-block-end:
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -143,24 +84,11 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
             _ -> Void in
             let textField = alertController.textFields![0] as UITextField
 
-            // :code-block-start: add-button-did-click
             
-            // :state-start: local sync
-            // Create a new Task with the text that the user entered.
-            let task = Task(name: textField.text ?? "New Task")
-
-            // Any writes to the Realm must occur in a write block.
-            try! self.realm.write {
-                // Add the Task to the Realm. That's it!
-                self.realm.add(task)
-            }
-            // :state-end: :state-uncomment-start: start
-            // // TODO: Replace the following code with code to create a Task instance and add it to the realm in a write block.
-            // let alertController = UIAlertController(title: "TODO", message: "Implement add task functionality in TasksViewController.swift", preferredStyle: .alert)
-            // alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-            // self.present(alertController, animated: true)
-            // :state-uncomment-end:
-            // :code-block-end:
+            // TODO: Replace the following code with code to create a Task instance and add it to the realm in a write block.
+            let alertController = UIAlertController(title: "TODO", message: "Implement add task functionality in TasksViewController.swift", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alertController, animated: true)
         }))
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         alertController.addTextField(configurationHandler: { (textField: UITextField!) -> Void in
@@ -179,40 +107,8 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         // Create the AlertController and add its actions.
         let actionSheet: UIAlertController = UIAlertController(title: task.name, message: "Select an action", preferredStyle: .actionSheet)
 
-        // :code-block-start: populate-action-sheet
-        // :state-start: local sync
-        // If the task is not in the Open state, we can set it to open. Otherwise, that action will not be available.
-        // We do this for the other two states -- InProgress and Complete.
-        if task.statusEnum != .Open {
-            actionSheet.addAction(UIAlertAction(title: "Open", style: .default) { _ in
-                    // Any modifications to managed objects must occur in a write block.
-                    // When we modify the Task's state, that change is automatically reflected in the realm.
-                    try! self.realm.write {
-                        task.statusEnum = .Open
-                    }
-                })
-        }
-
-        if task.statusEnum != .InProgress {
-            actionSheet.addAction(UIAlertAction(title: "Start Progress", style: .default) { _ in
-                    try! self.realm.write {
-                        task.statusEnum = .InProgress
-                    }
-                })
-        }
-
-        if task.statusEnum != .Complete {
-            actionSheet.addAction(UIAlertAction(title: "Complete", style: .default) { _ in
-                    try! self.realm.write {
-                        task.statusEnum = .Complete
-                    }
-                })
-        }
-        // :state-end: :state-uncomment-start: start
-        // // TODO: Populate the action sheet with task status update functions
-        // // for every state the task is not currently in.
-        // :state-uncomment-end:
-        // :code-block-end:
+        // TODO: Populate the action sheet with task status update functions
+        // for every state the task is not currently in.
 
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel) { _ in
                 actionSheet.dismiss(animated: true)
@@ -222,34 +118,17 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         self.present(actionSheet, animated: true, completion: nil)
     }
 
-    // :code-block-start: delete-task
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete else { return }
 
         // User can swipe to delete items.
         let task = tasks[indexPath.row]
 
-        // :state-start: local sync
-        // All modifications to a realm must happen in a write block.
-        try! realm.write {
-            // Delete the Task.
-            realm.delete(task)
-        }
-        // :state-end: :state-uncomment-start: start
-        // // TODO: Replace the following code with code to delete the task from the realm in a write block.
-        // let alertController = UIAlertController(title: "TODO", message: "Implement delete functionality in TasksViewController.swift", preferredStyle: .alert)
-        // alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-        // self.present(alertController, animated: true)
-        // :state-uncomment-end:
+        // TODO: Replace the following code with code to delete the task from the realm in a write block.
+        let alertController = UIAlertController(title: "TODO", message: "Implement delete functionality in TasksViewController.swift", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alertController, animated: true)
     }
-    // :code-block-end:
 
-    // :code-block-start: manage-button-did-click
-    // :state-uncomment-start: sync
-    // @objc func manageTeamButtonDidClick() {
-    //     present(UINavigationController(rootViewController: ManageTeamViewController()), animated: true)
-    // }
-    // :state-uncomment-end:
-    // :code-block-end:
 
 }
